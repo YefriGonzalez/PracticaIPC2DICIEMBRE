b7db69f09cd69e201f7228714fc8dea2cae721fc
Yefri González <yefrig00@gmail.com>
2021-12-09 01:52:15 -0600

    Modificacion login

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
-let nombre=document.getElementById("nombre");
+let nombre=document.getElementById("user");
 let pass=document.getElementById("password");
 
 function login(){
     console.log(nombre.value);
     console.log(pass.value);
+    alert(nombre.value);
 }
